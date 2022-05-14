63507eb4a7588f39e9e014c8379ed9f817fd9e97
rch-goldsnaker <rch.goldsnaker@gmail.com>
2022-05-14 15:58:37 -0300

    Changing link to portafolio

--- components/Layout/ContentFooter.vue
+++ components/Layout/ContentFooter.vue
@@ -4,7 +4,7 @@
       <ul class="nav">
         <li class="nav-item">
           <a
-            href="https://ioticos.org"
+            href="https://rch-goldsnaker.github.io/R006_Portafolio_NextJs/"
             target="_blank"
             rel="noopener"
             class="nav-link"
@@ -14,7 +14,7 @@
         </li>
         <li class="nav-item">
           <a
-            href="https://ioticos.org"
+            href="https://rch-goldsnaker.github.io/R006_Portafolio_NextJs/"
             target="_blank"
             rel="noopener"
             class="nav-link"
@@ -24,7 +24,7 @@
         </li>
         <li class="nav-item">
           <a
-            href="hhttps://ioticos.org"
+            href="https://rch-goldsnaker.github.io/R006_Portafolio_NextJs/"
             target="_blank"
             rel="noopener"
             class="nav-link"
@@ -37,7 +37,7 @@
         &copy; {{ year }}, made with <i class="tim-icons icon-heart-2"></i> by
         
         <a
-          href="https://ioticos.org"
+          href="https://rch-goldsnaker.github.io/R006_Portafolio_NextJs/"
           target="_blank"
           rel="noopener"
           >IoTicos</a
